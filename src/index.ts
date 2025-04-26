148c6702d690de6f8beb5508040936c22402ad51
metakunal <kunal99356@gmail.com>
2025-04-27 03:58:33 +0530

    Zerodha setup done with TS

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { KiteConnect } from "kiteconnect";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const apiKey = process.env.API_KEY||'';
+const apiSecret = process.env.API_SECRET||'';
+const requestToken = process.env.REQUEST_TOKEN||'';
+
+const kc = new KiteConnect({ api_key: apiKey });
+
+console.log(kc.getLoginURL());
+
+async function init() {
+  try {
+    await generateSession();
+    await getProfile();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function generateSession() {
+  try {
+    const response = await kc.generateSession(requestToken, apiSecret);
+    kc.setAccessToken(response.access_token);
+    console.log("Session generated:", response);
+  } catch (err) {
+    console.error("Error generating session:", err);
+  }
+}
+
+async function getProfile() {
+  try {
+    const profile = await kc.getProfile();
+    console.log("Profile:", profile);
+  } catch (err) {
+    console.error("Error getting profile:", err);
+  }
+}
+// Initialize the API calls
+init();
